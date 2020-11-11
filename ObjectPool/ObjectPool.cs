47d763dffc5fa5151a743ca297a1e9b24befbd4e
PumpK1n <30541379+ApumpK1n@users.noreply.github.com>
2020-11-11 22:08:32 +0800

    add

---
+++ ObjectPool/ObjectPool.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool<T> where T:MonoBehaviour
+{
+    private GameObject prefab;
+    private Stack<T> unUsedStack;
+    private int maxSize;
+
+    public ObjectPool(string prefabPath, int maxSize = -1, int originSize = 0):
+        this(Resources.Load<GameObject>(prefabPath), originSize)
+    {
+    }
+
+    public ObjectPool(GameObject obj, int maxSize = -1, int originSize = 0)
+    {
+        prefab = obj;
+        unUsedStack = new Stack<T>();
+
+        for (int i = 0; i < originSize; i++)
+        {
+            unUsedStack.Push(CreateItem());
+        }
+
+        if (maxSize < 0)
+        {
+            this.maxSize = maxSize;
+        }
+        else
+        {
+            this.maxSize = Math.Max(originSize, maxSize);
+        }
+    }
+    
+    private T CreateItem()
+    {
+        GameObject obj = UnityEngine.Object.Instantiate<GameObject>(prefab);
+        T component = obj.transform.GetComponent<T>();
+
+        return component;
+    }
+    
+    public T Allocate()
+    {
+        if (unUsedStack.Count > 0)
+        {
+            return unUsedStack.Pop();
+        }
+        else
+        {
+            return CreateItem();
+        }
+    }
+
+    public void Recycle(T item)
+    {
+        item.transform.parent = null;
+        if (maxSize < 0)
+        {
+            unUsedStack.Push(item);
+        }
+        else
+        {
+            if (GetPoolSize() < maxSize)
+            {
+                unUsedStack.Push(item);
+            }
+            else
+            {
+                UnityEngine.Object.Destroy(item.gameObject);
+            }
+        }
+    }
+
+    public int GetPoolSize()
+    {
+        return unUsedStack.Count;
+    }
+
+    public void Release()
+    {
+        T[] list = unUsedStack.ToArray();
+        for (int i = 0; i < list.Length; i++)
+        {
+            UnityEngine.Object.Destroy(list[i].gameObject);
+        }
+        unUsedStack.Clear();
+    }
+}
---
+++ ObjectPool/PoolManager.cs
@@ -0,0 +1,43 @@
+﻿using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager
+{
+    private static PoolManager instance;
+
+    public static PoolManager Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                instance = new PoolManager();
+            }
+
+            return instance;
+        }
+    }
+
+    private Dictionary<string, ObjectPool<MonoBehaviour>> pools;
+
+    private PoolManager()
+    {
+        pools = new Dictionary<string, ObjectPool<MonoBehaviour>>();
+    }
+
+    public void Allocate(string poolName)
+    {
+        
+    }
+
+    public void Recycle<T>(T item) where T:MonoBehaviour
+    {
+        string key = item.name;
+        if (!pools.ContainsKey(key))
+        {
+            //pools.Add(key, new ObjectPool<T>(item.gameObject));
+        }
+    }
+}
